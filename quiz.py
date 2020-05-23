8d81d96dc066b378946479ff64c147232932cdcb
nikolya98 <nikolya.knyazev.98@mail.ru>
2020-05-23 16:03:08 +0300

    create 'get_themes()' and change the logic of 'thematic_qiuz()'

--- quiz.py
+++ quiz.py
@@ -4,9 +4,15 @@ import time
 from db_create import create_thematic_db, create_difficult_db
 
 
-def thematic_quiz():
-    create_thematic_db()
-    db = sqlite3.connect('data_bases/thematic_quiz.db')
+def get_themes(mode=None):
+    if mode == 'thematic':
+        path = 'data_bases/thematic_quiz.db'
+        create_thematic_db()
+    if mode == 'difficult':
+        path = 'data_bases/difficult_quiz.db'
+        create_difficult_db()
+
+    db = sqlite3.connect(path)
     sql = db.cursor()
 
     sql.execute(f"""SELECT name FROM sqlite_master WHERE type = 'table'""")
@@ -16,47 +22,52 @@ def thematic_quiz():
         for elem in name:
             themes.append(elem)
 
-    choose_theme = random.sample(themes, 2)  # Предлагаем тему на выбор.... Добавить больше тем для викторин!!!
-    for theme in themes:
-        print(f'{themes.index(theme) + 1}. {theme}')
+    return themes
 
-    while True:
-        user_choice = input('Выберите тему: ')  # Переменная для выбора пользователя...
-        if user_choice in choose_theme:
-            break
-        else:
-            print('Такой темы нет! Выберите из списка....')
+
+def thematic_quiz(user_choice):
+    db = sqlite3.connect('data_bases/thematic_quiz.db')
+    sql = db.cursor()
+
+    ##########################
+    # num_of_themes = len(themes)
+    # if num_of_themes < 4:
+    #     choose_theme = random.sample(themes, num_of_themes)
+    # else:
+    #     choose_theme = random.sample(themes, 4)
+    ##########################
 
     sql.execute(f"""SELECT * FROM {user_choice} ORDER BY RANDOM() LIMIT 5""")
-
     records = sql.fetchall()
-    good_attempts = 0
-    start = time.time()
+
+    ##########################
+    # good_attempts = 0
+    # start = time.time()
+    ##########################
+
     for record in records:
         question, prompts, answer = record
         prompts = prompts.split(',')
         random.shuffle(prompts)
-        print(f'{question}')
-        for prompt in prompts:
-            print(f'{prompt}')
-        user_answer = input('Выберите ответ: ')
-        if user_answer == answer:
-            good_attempts += 1
-    print(f'Угадано {good_attempts} из {len(records)}. Время потрачено: {time.time() - start}')
+        yield question, prompts, answer
+
+    ##########################
+    #     print(f'{question}')
+    #     for prompt in prompts:
+    #         print(f'{prompt}')
+    #     user_answer = input('Выберите ответ: ')
+    #     if user_answer == answer:
+    #         good_attempts += 1
+    # print(f'Угадано {good_attempts} из {len(records)}. Время потрачено: {time.time() - start}')
+    ##########################
+
     db.close()
 
 
 def difficult_quiz():
-    create_difficult_db()
     db = sqlite3.connect('data_bases/difficult_quiz.db')
     sql = db.cursor()
-
-    sql.execute(f"""SELECT name FROM sqlite_master WHERE type = 'table'""")
-    tables_name = sql.fetchall()
-    themes = []
-    for name in tables_name:
-        for elem in name:
-            themes.append(elem)
+    themes = get_themes('difficult')
 
     curr_theme = random.choice(themes)
     sql.execute(f"""SELECT * FROM {curr_theme} ORDER BY RANDOM() LIMIT 15""")
@@ -75,12 +86,9 @@ def difficult_quiz():
     db.close()
 
 
-def quiz(thematic=False, difficult=False):
-    if thematic:
-        thematic_quiz()
-
-    if difficult:
-        difficult_quiz()
-
-
-quiz(thematic=True)
+# def quiz(thematic=False, difficult=False):
+#     if thematic:
+#         thematic_quiz()
+#
+#     if difficult:
+#         difficult_quiz()
