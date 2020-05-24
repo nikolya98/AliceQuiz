888ca8a08a4d82a13ff5af235a021af4925810b5
nikolya98 <nikolya.knyazev.98@mail.ru>
2020-05-24 14:21:56 +0300

    deleted unnecessary parts of the code

--- quiz.py
+++ quiz.py
@@ -1,6 +1,5 @@
 import sqlite3
 import random
-import time
 from db_create import create_thematic_db, create_difficult_db
 
 
@@ -29,37 +28,14 @@ def thematic_quiz(user_choice):
     db = sqlite3.connect('data_bases/thematic_quiz.db')
     sql = db.cursor()
 
-    ##########################
-    # num_of_themes = len(themes)
-    # if num_of_themes < 4:
-    #     choose_theme = random.sample(themes, num_of_themes)
-    # else:
-    #     choose_theme = random.sample(themes, 4)
-    ##########################
-
     sql.execute(f"""SELECT * FROM {user_choice} ORDER BY RANDOM() LIMIT 5""")
     records = sql.fetchall()
-
-    ##########################
-    # good_attempts = 0
-    # start = time.time()
-    ##########################
 
     for record in records:
         question, prompts, answer = record
         prompts = prompts.split(',')
         random.shuffle(prompts)
         yield question, prompts, answer
-
-    ##########################
-    #     print(f'{question}')
-    #     for prompt in prompts:
-    #         print(f'{prompt}')
-    #     user_answer = input('Выберите ответ: ')
-    #     if user_answer == answer:
-    #         good_attempts += 1
-    # print(f'Угадано {good_attempts} из {len(records)}. Время потрачено: {time.time() - start}')
-    ##########################
 
     db.close()
 
@@ -73,23 +49,9 @@ def difficult_quiz():
     sql.execute(f"""SELECT * FROM {curr_theme} ORDER BY RANDOM() LIMIT 15""")
     records = sql.fetchall()
 
-    ##########################
-    # good_attempts = 0
-    # start = time.time()
-    ##########################
-
     for record in records:
         question, answer = record
         question, answer = question.replace('\\xa0', ' '), answer.replace('\\xa0', ' ') # Исправить проблему с неразрывными пробелами
         yield question, answer
-
-    ##########################
-    #     print(question)
-    #     user_answer = input('Введите ответ: ')
-    #     if user_answer == answer:
-    #         good_attempts += 1
-    #     print(f'Правильный ответ {answer}')
-    # print(f'Угадано {good_attempts} из {len(records)}. Время потрачено: {time.time() - start}')
-    ##########################
 
     db.close()
